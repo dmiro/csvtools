d9657b6b1c717f4be0f90fb9c7318e619de080a1
dmiro <davidmirom@hotmail.com>
2016-02-26 09:12:04 +0100

    0.0.95

--- src/lib/helpers/iniconfig.py
+++ src/lib/helpers/iniconfig.py
@@ -11,62 +11,58 @@ class IniConfig(object):
     @classmethod
     def booleanProperty(cls, section, option, default):
         def getter(self):
-            return self.__getBoolean(section, option, default)
+            return self.__get(section, option, default,
+                              self.__config.getboolean)
         def setter(self, value):
             self.__set(section, option, value)
-            #self.__save()
         return property(getter, setter)
 
     @classmethod
     def intProperty(cls, section, option, default):
         def getter(self):
-            return self.__getInt(section, option, default)
+            return self.__get(section, option, default,
+                              self.__config.getint)
         def setter(self, value):
             self.__set(section, option, value)
-            #self.__save()
         return property(getter, setter)
 
     @classmethod
     def jsonProperty(cls, section, option, default):
         def getter(self):
-            return json.loads(self.__get(section, option, default))
+            return json.loads(self.__get(section, option, default,
+                                         self.__config.get))
         def setter(self, value):
             self.__set(section, option, json.dumps(value))
-            #self.__save()
         return property(getter, setter)
 
     #
     # private
     #
 
-    def __getBoolean(self, section, option, default):
-        self.__load()
-        if not self.__config.has_section(section):
-            self.__config.add_section(section)
-        if self.__config.has_option(section, option):
-            return self.__config.getboolean(section, option)
-        else:
-            return default
+    def __getProperty(self, section, option):
+        attrNameMangled = '_' + self.__class__.__name__ + '__' + section + '_' + option
+        if hasattr(self, attrNameMangled):
+            return getattr(self, attrNameMangled)
+        return None
 
-    def __getInt(self, section, option, default):
-        self.__load()
-        if not self.__config.has_section(section):
-            self.__config.add_section(section)
-        if self.__config.has_option(section, option):
-            return self.__config.getint(section, option)
-        else:
-            return default
+    def __setProperty(self, section, option, value):
+        attrNameMangled = '_' + self.__class__.__name__ + '__' + section + '_' + option
+        setattr(self, attrNameMangled, value)
 
-    def __get(self, section, option, default):
-        self.__load()
-        if not self.__config.has_section(section):
-            self.__config.add_section(section)
-        if self.__config.has_option(section, option):
-            return self.__config.get(section, option)
-        else:
-            return default
+    def __get(self, section, option, default, configGetter):
+        value = self.__getProperty(section, option)
+        if not value:
+            if not self.__config.has_section(section):
+                self.__config.add_section(section)
+            if self.__config.has_option(section, option):
+                value = configGetter(section, option)
+            else:
+                value = default
+            self.__setProperty(section, option, value)
+        return value
 
     def __set(self, section, option, value):
+        self.__setProperty(section, option, value)
         if not self.__config.has_section(section):
             self.__config.add_section(section)
         self.__config.set(section, option, value)
@@ -87,4 +83,4 @@ class IniConfig(object):
         super(IniConfig, self).__init__()
         self.filename = filename
         self.__config = ConfigParser.ConfigParser()
-        #self.__load()
+        self.__load()
