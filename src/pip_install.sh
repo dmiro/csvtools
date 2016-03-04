6fdf6fe9bce3c3f32f2ad04a10b4c44d7c2ebe13
dmiro <davidmirom@hotmail.com>
2016-03-04 21:54:00 +0100

    0.0.102

--- src/pip_install.sh
+++ src/pip_install.sh
@@ -1,2 +1,8 @@
+rem 1.
+rem download & install PyQt4 32 bit from:  
+rem http://sourceforge.net/projects/pyqt/files/PyQt4/PyQt-4.11.4/PyQt4-4.11.4-gpl-Py2.7-Qt4.8.7-x32.exe
+
+rem 2.
+rem install xlrd to load xsl/xslt.
 rem https://pypi.python.org/pypi/xlrd
 pip install xlrd
